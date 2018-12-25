db3e4d70f00d0080979f934979baa1a5e5e56378
Spexx <44988681+Spexxtacular@users.noreply.github.com>
2018-12-25 22:48:13 +0000

    hug is no longer guild specifc

--- commands/hug.js
+++ commands/hug.js
@@ -1,8 +1,6 @@
 module.exports = {
     name: 'hug',
     hidden: true,
-    guildOnly: 1,
-    guild: ['489939754021027841', '476904255815548969', '513806689787445261'],
     execute(msg, args) {
       if (Math.floor(Math.random() * 1000) + 1 === 414) {
         sendMsg('wat');
