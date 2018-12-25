e8f939d0e0415187626ee0da20153bd977a17edd
Spexx <44988681+Spexxtacular@users.noreply.github.com>
2018-12-25 01:27:44 +0000

    removed double log

--- commands/pet.js
+++ commands/pet.js
@@ -6,7 +6,6 @@ module.exports = {
     guild: '489939754021027841',
     execute(msg, args) {        
         sendMsg('Please don\'t pet me! <:Isabadday:490255608923291660>');
-        sendLog(msg.author.tag + ' petted Isabelle');
         sendLog(msg.author.tag + ' tried to pet Isabelle in ' + msg.guild.name);
     }
 };
