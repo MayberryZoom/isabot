fc7b7881803bfbfef4a56677b1b775123f8146e0
Spexx <spexxsmash@gmail.com>
2019-01-07 15:52:57 -0500

    NOW (snake in >data)

--- commands/hug.js
+++ commands/hug.js
@@ -2,9 +2,11 @@ module.exports = {
     name: 'hug',
     hidden: true,
     execute(msg, args) {
-      if (Math.floor(Math.random() * 950) + 1 == 414) {
+      let num = Math.floor(Math.random() * 950) + 1;
+
+      if (num == 414) {
         sendMsg('wat');
       }
-      sendLog(msg.author.tag + ' failed to hug Isabelle in ' + msg.guild.name);
+      sendLog(msg.author.tag + ' failed to hug Isabelle in ' + msg.guild.name + '. ``num`` was ' + num);
     }
 };
