072f5f5e937b11248c79b129a69cba62b4fe603c
Vincent Del Vecchio <N00787794@students.ncc.edu>
2019-01-07 23:32:35 -0500

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -3,7 +3,7 @@ module.exports = {
     hidden: true,
     execute(msg, args) {
         
-      if(msg.member.roles.has(&502544290765864970))
+      if(msg.member.roles.has("502544290765864970"))
          {
             let num = 414;
          }
