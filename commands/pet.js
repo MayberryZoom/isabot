ac4f14c2d28367d54dc6ad0f154f005271f9f027
Spexx <44988681+Spexxtacular@users.noreply.github.com>
2019-04-02 08:53:36 -0400

    fixing emote

--- commands/pet.js
+++ commands/pet.js
@@ -6,7 +6,7 @@ module.exports = {
     dmDisabled: 1,
     guilds: ['489939754021027841'],
     execute(msg, args) {
-        msg.channel.send('Please don\'t pet me! <:Isabadday:490255608923291660>')
+        msg.channel.send('Please don\'t pet me! <:isaBadDay:562485951558057984>')
         .then(() => { return sendLog(msg.author.tag + ' petted Isabelle (and she didn\'t like it)') });
     }
 };
