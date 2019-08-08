a273e0ea4187a0b3f83951c6ee9de4382942fb9c
Spexx <spexxsmash@gmail.com>
2019-08-08 02:23:45 -0400

    oops

--- commands/avatar.js
+++ commands/avatar.js
@@ -8,8 +8,8 @@ module.exports = {
     execute(msg, args) {
         return new Promise(async (resolve, reject) => {
             const user = await parseUser(msg, args.join(' '));
-            if (!user) return msg.channel.send('That user is not valid!').then(resolve).catch(e => reject(e));
-            return msg.channel.send(new Discord.RichEmbed() .setTitle(user.username + "'s avatar") .setImage(user.avatarURL) .setColor(isabotColor) .setFooter('Requested by ' + msg.author.tag, msg.author.avatarURL) .setTimestamp()).then(resolve).catch(e => reject(e));
+            if (!user) return msg.channel.send('That user is not valid!').then(resolve()).catch(e => reject(e));
+            return msg.channel.send(new Discord.RichEmbed() .setTitle(user.username + "'s avatar") .setImage(user.avatarURL) .setColor(isabotColor) .setFooter('Requested by ' + msg.author.tag, msg.author.avatarURL) .setTimestamp()).then(resolve()).catch(e => reject(e));
         })
     }
 };
