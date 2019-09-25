117bbbe8b8aff9575fa3c9eff3cf573e11e3a395
Spexx <spexxsmash@gmail.com>
2019-09-25 11:01:34 -0400

    >define ignores spaces/dashes/underscores and has alias support

--- template.js
+++ template.js
@@ -11,14 +11,18 @@ module.exports = {
     args: false,
     // Specifies if the command is hidden from >help
     hidden: false,
+    // Specifies if the command is exclusive to owners
+    ownerOnly: false,
     // Specifies if the command is enabled in DMs
     // undefined = enabled, 1 = response then ignore command, 2 = no response then ignore command
     dmDisabled: 0,
     // Array of guild IDs this is exclusive to
     guilds: [],
-    // Permissions required by the bot to execute this command
+    // Command category used for >help
+    category: '',
+    // Permissions required by the bot to execute this command (not working atm)
     botPermissions: [],
-    // Permissions required by the user to execute this command
+    // Permissions required by the user to execute this command (not working atm)
     userPermissions: [],
     // Command itself
     execute(msg, args) {
