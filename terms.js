913b92f99ee4b3bce1cf735c0a7148f794bc0efa
rowcla <31002885+rowcla@users.noreply.github.com>
2019-12-28 18:50:28 +1100

    Update terms.js

--- terms.js
+++ terms.js
@@ -560,7 +560,6 @@ module.exports = [
    },
    {
       name: 'run state storage',
-      aliases: ['run walk storage'],
       definition: 'When forward is held on the control stick, nana and popo maintain their current state of running or walking./n/nRun state storage is where you get popo to walk and nana to run. In this state, releasing the stick cause only nana to enter skid for 12 frames during which she cannot dash backwards or shield. This allows you to dash backwards or shield with popo and immediately do a nana first desync./n/nThis state can be achieved from any previous desync./n/nIf only popo can act, you can quickly mash forward twice quickly and then continue to hold forward 6-12 frames before nana can act to cause popo to do a dashwalk and nana to dash which converts into run state storage./n/nWhen nana can act, you can simply hold forward more than 6 frames before popo can act. This causes nana to dash and popo to walk./n/nExamples:/n<https://twitter.com/madmonk12345/status/1188207819780050944?s=21>/n<https://twitter.com/madmonk12345/status/1195622276680433664?s=21>',
       character: 'ice climbers'
    },
