f0277f817848c6d9ca6146763da652f578372b4e
Izzychu <spexxsmash@gmail.com>
2021-07-10 18:38:42 -0400

    changelog

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,23 +1,17 @@
-const changes = `__4/4/2021__
+const changes = `__7/10/21__
+- Updated \`>define\`
+    - Updated old, out of date terms
+    - Improved readability and explanations of old terms
+    - Added new general terms
+    - Added new terms for Pyra/Mythra
+
+__4/4/2021__
 - Updated to discord.js v12
     - What does this mean? It means the bot is more stable, several bugs were fixed, and just overall optimization. This will also make future updates easier.
 
 __3/15/2021__
 - Added hitbox data for Pyra/Mythra (except Mythra specials)
-- Added attributes for Pyra/Mythra
-
-__2/19/2021__
-- Added hitbox data for Sephiroth
-- Added Pyra/Mythra to \`>render\`
-
-__12/17/2020__
-- Updated \`>stat\`
-    - Added attributes for Sephiroth
-    - Fixed Steve's attribute data (was accidentally set to Min-Min's)
-
-__12/11/2020__
-- Added hitbox data for Steve (except specials)
-- Added Sephiroth to \`>render\``;
+- Added attributes for Pyra/Mythra`
 
 module.exports = {
     name: 'changelog',
