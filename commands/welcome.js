fc1738ab7bd702e437faa581f36ca2398df24141
Izzychu <izzychu222@gmail.com>
2022-08-27 16:56:05 -0400

    Port welcome command

--- commands/welcome.js
+++ commands/welcome.js
@@ -1,21 +1,17 @@
-const conversions = require('../conversions.js');
-
 module.exports = {
-    name: 'welcome',
-    description: 'Welcome!',
-    usage: ['<user>'],
-    dmDisabled: 1,
-    execute(msg, args) {
+    data: new Discord.SlashCommandBuilder()
+        .setName('welcome')
+        .setDescription('Welcome a user!')
+        .setDMPermission(false)
+        .addUserOption(option => 
+            option.setName('user')
+                .setDescription('The user to welcome')),
+    execute(interaction) {
         return new Promise (async (resolve, reject) => {
-            let text;
-            if (args[0]) {
-                text = await conversions.parseUser(msg, args.join(' '));
-                text = ' ' + text.username;
-            }
-            if (!text) text = '';
-            msg.channel.send(`Welcome${text}! <:isaHeyyy:490255578061602828>`)
-            .then(resolve())
-            .catch((e) => reject(e));
+            let user = interaction.options.getUser('user');
+            user = user ? ' ' + user.username : ''
+
+            interaction.reply('Welcome' + user + '! <:isaHeyyy:490255578061602828>').then(resolve()).catch((e) => reject(e));
         });
     }
 };
