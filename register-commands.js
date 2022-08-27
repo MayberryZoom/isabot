f6d034f327849086325397222a472113eb8e2b2e
Izzychu <izzychu222@gmail.com>
2022-08-27 12:27:40 -0400

    Dynamically register guild commands

--- register-commands.js
+++ register-commands.js
@@ -1,16 +1,21 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
 const { token } = require('./token.json');
+global.Discord = require('discord.js')
 const fs = require('fs');
 
 const clientId = '513515391155175424';
 
-let commands = [];
+let globalCommands = [];
+let guildCommands = {};
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+	if (command.data) {
+		if (command.guilds) command.guilds.map(g => guildCommands[g] ? guildCommands[g].push(command.data.toJSON()) : guildCommands[g] = [command.data.toJSON()])
+		else globalCommands.push(command.data.toJSON());
+	}
 }
 
 const rest = new REST({ version: '10' }).setToken(token);
@@ -19,7 +24,9 @@ const rest = new REST({ version: '10' }).setToken(token);
 	try {
 		console.log('Started refreshing application commands.');
 
-		await rest.put(Routes.applicationCommands(clientId), { body: commands });
+		await rest.put(Routes.applicationCommands(clientId), { body: globalCommands });
+
+		Object.keys(guildCommands).map(g => rest.put(Routes.applicationGuildCommands(clientId, g), { body: guildCommands[g] }));
 
 		console.log('Successfully reloaded application commands.');
 	}
