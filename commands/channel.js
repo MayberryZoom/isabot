9d5137ad86df71f8dde1f4a3a49acde704834a2a
Izzychu <izzychu222@gmail.com>
2022-08-27 13:00:31 -0400

    Port channel command

--- commands/channel.js
+++ commands/channel.js
@@ -1,26 +1,27 @@
 module.exports = {
-    name: 'channel',
-    description: 'Shows information about a channel. If no  are provided, information will be shown about the current channel.',
-    usage: ['<channel>'],
-    dmDisabled: 1,
-    category: 'info',
-    execute(msg, args) {
+    data: new Discord.SlashCommandBuilder()
+        .setName('channel')
+        .setDescription('Shows information about a channel. Defaults to current channel')
+        .addChannelOption(option => 
+            option.setName('channel')
+                .setDescription('The channel to fetch info about')),
+    execute(interaction) {
         return new Promise(async (resolve, reject) => {
-            const conversions = require('../conversions.js');
+            let channel = interaction.options.getChannel('channel');
+            if (!channel) channel = interaction.channel;
 
-            const c = await conversions.parseChannel(msg, args.join(' '));
             const embed = new Discord.EmbedBuilder()
-                .setTitle(c.name + ' (' + c.id + ')' + (c.nsfw ? ' ⚠' : ''))
+                .setTitle(channel.name + ' (' + channel.id + ')' + (channel.nsfw ? ' ⚠' : ''))
                 .setColor(isabotColor)
                 .addFields(
-                    { name: 'Created At', value: c.createdAt.toUTCString() },
-                    { name: 'Category', value: c.parent.name, inline: true },
-                    { name: 'Channel Topic', value: c.topic ? c.topic : 'No channel topic', inline: true },
-                    { name: 'Members', value: c.members.size.toString(), inline: true }
+                    { name: 'Created At', value: channel.createdAt.toUTCString() },
+                    { name: 'Category', value: channel.parent.name, inline: true },
+                    { name: 'Channel Topic', value: channel.topic ? channel.topic : 'No channel topic', inline: true },
+                    { name: 'Members', value: channel.members.size.toString(), inline: true }
                 )
                 .setTimestamp();
 
-            msg.channel.send({ embeds: [embed] }).then(resolve()).catch((e) => reject(e));
+            interaction.reply({ embeds: [embed] }).then(resolve()).catch((e) => reject(e));
         });
     }
 };
