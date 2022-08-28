1f0088061832571c429a4298062e8c599098018f
Izzychu <izzychu222@gmail.com>
2022-08-28 15:21:56 -0400

    Port render command

--- commands/render.js
+++ commands/render.js
@@ -7,25 +7,29 @@ const toUnderscore = (text, split) => {
 }
 
 module.exports = {
-    name: 'render',
-    description: 'Gets a character\'s render in SSBU. When adding an alternate costume, use 1-8.',
-    usage: ['<character> <alt>'],
-    args: true,
-    category: 'smash',
-    execute(msg, args) {
+    data: new Discord.SlashCommandBuilder()
+        .setName('render')
+        .setDescription('Gets character renders in Smash Ultimate.')
+        .addStringOption(option =>
+            option.setName('character')
+                .setDescription('Character to fetch render of')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('costume')
+                .setDescription('Alternate costume to fetch')
+                .setMinValue(1)
+                .setMaxValue(8)),
+    execute(interaction) {
         return new Promise((resolve, reject) => {
-            let character, x = args.length;
-            while (!character && x >= 0) {
-                character = toOneWord(args.slice(0, x).join(' ').toLowerCase());
-                character = characters.find(c => toOneWord(c.name) === character || (c.aliases && c.aliases.map(x => toOneWord(x)).includes(character)));
-                x--;
-            }
-            if (!character || character.name === 'luma') return msg.channel.send('That character is not valid!').then(resolve()).catch(e => reject(e));
+            let character = toOneWord(interaction.options.getString('character').toLowerCase());
+            character = characters.find(c => toOneWord(c.name) === character || (c.aliases && c.aliases.map(x => toOneWord(x)).includes(character)));
+            if (!character || character.name === 'luma') return interaction.reply('That character is not valid!').then(resolve()).catch(e => reject(e));
 
             const nameToUse = character.ssbuLink ? character.ssbuLink : character.name;
             const charNameFixed = toUnderscore(nameToUse.replace(/\./g, '').replace(/&/g, 'and'), ['-', ' ']);
-            let alt = args.slice(x + 1);
-            alt = alt && alt < 9 && alt > 1 ? 'main' + alt : 'main';
+
+            let alt = interaction.options.getInteger('costume');
+            alt = alt ? 'main' + alt : 'main';
 
             const embed = new Discord.EmbedBuilder()
                 .setColor(character.color)
@@ -33,7 +37,7 @@ module.exports = {
                 .setTitle(capitalize(character.name, [' ', '-', '.']))
                 .setTimestamp();
 
-            return msg.channel.send({ embeds: [embed] }).then(resolve()).catch((e) => reject(e));
+            interaction.reply({ embeds: [embed] }).then(resolve()).catch((e) => reject(e));
         });
     }
 };
