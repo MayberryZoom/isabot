04ffcfa814f4b3f76377bcbbe67befb9e4a50a9f
Izzychu <izzychu222@gmail.com>
2022-08-28 21:47:09 -0400

    Update changelog

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,22 +1,11 @@
-const changes = `__10/20/2021__
-- Added Sora attributes
-
-__7/17/2021__
-- Added terms for sharking and pancaking
-
-__7/16/2021__
-- Optimization to parsing algorithm. Now accounts for any permutation of words, meaning it will be easier to find the desired result, and file size is cut down
-- Added special hitbox data for Meta Knight, Bayonetta, Byleth, Ice Climbers, Fox, Falco, Wolf, Captain Falcon, Ganondorf, Corrin, Ike, and Jigglypuff
-- Added getup attacks, ledge attacks, ledge hangs, and standard ledge grab ranges for every character
-
-__7/13/2021__
-- Added special hitbox data for Mario, Luigi, Dr. Mario, Bowser, Mewtwo, and Mega Man
-
-__7/12/2021__
-- Added hitbox data for Mythra specials
-- Added hitbox data for Kazuya
-- Added attributes for Kazuya
-- Added Kazuya to \`>render\``
+const changes = `__8/28/2022__
+**Isabot version 2.0.0!**
+- Various bug fixes/improvements
+- Prefix commands are now deprecated
+- Slash commands now fully supported, please use these instead
+    - The migration to slash commands also means a better user experience!
+    - Users should find it much easier to use the bot in general now
+    - Many commands have been significantly simplified, both in code and usage`
 
 module.exports = {
     data: new Discord.SlashCommandBuilder()
