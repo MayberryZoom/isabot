0bdb084b2b0a5963fec22a51e5c7336c70dbc989
Mayberry <mayberryzoom@gmail.com>
2024-09-09 12:32:06 -0400

    Update changelog

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,11 +1,5 @@
-const changes = `__8/28/2022__
-**Isabot version 2.0.0!**
-- Various bug fixes/improvements
-- Prefix commands are now deprecated
-- Slash commands now fully supported, please use these instead
-    - The migration to slash commands also means a better user experience!
-    - Users should find it much easier to use the bot in general now
-    - Many commands have been significantly simplified, both in code and usage`
+const changes = `__09/09/2024__
+Updated outdated/broken Twitter links`
 
 module.exports = {
     data: new Discord.SlashCommandBuilder()
