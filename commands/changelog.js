3295c8e0ae9062bd52c4459193aa9e8d9172d477
Mayberry <mayberryzoom@gmail.com>
2024-09-10 15:32:56 -0400

    Update changelog

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,5 +1,8 @@
-const changes = `__09/09/2024__
-Updated outdated/broken Twitter links`
+const changes = `__09/10/2024__
+- Removed \`/isabelle\` and \`/digby\`
+
+__09/09/2024__
+- Updated outdated/broken Twitter links`
 
 module.exports = {
     data: new Discord.SlashCommandBuilder()
